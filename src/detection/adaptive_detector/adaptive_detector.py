efdacf41763677d6ca56f5c4f9686bf10c132398
Miranda84315 <miranda84315@gmail.com>
2019-02-15 17:44:52 +0800

    finish show openpose detection

--- src/detection/adaptive_detector/adaptive_detector.py
+++ src/detection/adaptive_detector/adaptive_detector.py
@@ -2,23 +2,27 @@ import numpy as np
 import cv2
 import scipy.io
 import h5py
+import math
 
-detection_root = ['D:/Code/TrackingSystem/dataset/detections/faster_rcnn/', 'D:/Code/TrackingSystem/dataset/detections/openpose/']
+detection_root = [
+    'D:/Code/TrackingSystem/dataset/detections/faster_rcnn/',
+    'D:/Code/TrackingSystem/dataset/detections/openpose/'
+]
 start_time = [5543, 3607, 27244, 31182, 1, 22402, 18968, 46766]
 NumFrames = [359580, 360720, 355380, 374850, 366390, 344400, 337680, 353220]
-PartFrames = [
-    [38370, 38370, 38400, 38670, 38370, 38400, 38790, 38370], 
-    [38370, 38370, 38370, 38670, 38370, 38370, 38640, 38370], 
-    [38370, 38370, 38370, 38670, 38370, 38370, 38460, 38370], 
-    [38370, 38370, 38370, 38670, 38370, 38370, 38610, 38370], 
-    [38370, 38370, 38370, 38670, 38370, 38400, 38760, 38370], 
-    [38370, 38370, 38370, 38700, 38370, 38400, 38760, 38370], 
-    [38370, 38370, 38370, 38670, 38370, 38370, 38790, 38370],
-    [38370, 38370, 38370, 38670, 38370, 38370, 38490, 38370],
-    [38370, 38370, 38370, 38670, 38370, 37350, 28380, 38370], 
-    [14250, 15390, 10020, 26790, 21060, 0, 0, 7890]]
+PartFrames = [[38370, 38370, 38400, 38670, 38370, 38400, 38790, 38370], [
+    38370, 38370, 38370, 38670, 38370, 38370, 38640, 38370
+], [38370, 38370, 38370, 38670, 38370, 38370, 38460,
+    38370], [38370, 38370, 38370, 38670, 38370, 38370, 38610, 38370], [
+        38370, 38370, 38370, 38670, 38370, 38400, 38760, 38370
+    ], [38370, 38370, 38370, 38700, 38370, 38400, 38760,
+        38370], [38370, 38370, 38370, 38670, 38370, 38370, 38790, 38370],
+              [38370, 38370, 38370, 38670, 38370, 38370, 38490, 38370],
+              [38370, 38370, 38370, 38670, 38370, 37350, 28380,
+               38370], [14250, 15390, 10020, 26790, 21060, 0, 0, 7890]]
 start_sequence = 127720
 end_sequence = 187540
+
 
 def calucate_part(icam, frame):
     sum_frame = 0
@@ -41,20 +45,52 @@ def load_detection_h5py(detection_path):
     return data
 
 
-def draw_pose(pose, img):
-
+def draw_pose(poses, img):
+    height = img.shape[0]
+    width = img.shape[1]
+    pose_threshold = 0.05
+    # each node's connect (1,2) (1,5) (2,3) (3,4) ...
+    COCO_connect = [
+        1, 2, 1, 5, 2, 3, 3, 4, 5, 6, 6, 7, 1, 8, 8, 9, 9, 10, 1, 11, 11, 12,
+        12, 13, 1, 0, 0, 14, 14, 16, 0, 15, 15, 17
+    ]
+    COCO_color = [
+        (255, 0, 85), (255, 0, 0), (255, 85, 0), (255, 170, 0), (255, 255, 0), (170, 255, 0),
+        (85, 255, 0), (0, 255, 0), (0, 255, 85), (0, 255, 170), (0, 255, 255), (0, 170, 255),
+        (0, 85, 255), (0, 0, 255), (255, 0, 170), (170, 0, 255), (255, 0, 255), (85, 0, 255)]
+    for pose in poses:
+        for pair in range(0, int(len(COCO_connect) / 2)):
+            # check if the connect is not valid
+            if (
+                pose[3 * COCO_connect[pair * 2] + 2] < pose_threshold or
+                pose[3 * COCO_connect[pair * 2] + 1] == 0 or
+                pose[3 * COCO_connect[pair * 2] + 0] == 0 or
+                pose[3 * COCO_connect[pair * 2 + 1] + 2] < pose_threshold or
+                pose[3 * COCO_connect[pair * 2 + 1] + 1] == 0 or
+                pose[3 * COCO_connect[pair * 2 + 1] + 0] == 0):
+                continue
+            pt1 = (int(width * pose[3 * COCO_connect[pair * 2 + 1]]), int(height * pose[3 * COCO_connect[pair * 2 + 1] + 1]))
+            pt2 = (int(width * pose[3 * COCO_connect[pair * 2]]), int(height * pose[3 * COCO_connect[pair * 2] + 1]))
+            color = COCO_color[pair % 18]
+            img = cv2.line(img, pt1, pt2, color, 3)
+    return img
 
 
 def show_detections(detection, iCam, frame):
     part_cam, part_frame = calucate_part(iCam, frame)
-    filename = 'D:/Code/DukeMTMC/videos/camera' + str(iCam) + '/0000' + str(part_cam) + '.MTS'
+    filename = 'D:/Code/DukeMTMC/videos/camera' + str(iCam) + '/0000' + str(
+        part_cam) + '.MTS'
     cap = cv2.VideoCapture(filename)
     cap.set(1, part_frame)
     _, img = cap.read()
     # cv2.imshow("video", img)
     # cv2.waitKey(1)
-    detections = [detection[i] for i in range(0, len(detection)) if detection[i][1] == frame]
-    img_pose = draw_pose(detections, img)
+    detections = [
+        detection[i] for i in range(0, len(detection))
+        if detection[i][1] == frame
+    ]
+    detection = np.array(detections)[:, 2:]
+    img_pose = draw_pose(detection, img)
 
     return img_pose
 
@@ -71,11 +107,17 @@ def main():
     start = start_sequence - start_time[iCam - 1] + 1
     end = end_sequence - start_time[iCam - 1] + 1
 
-    # because in faster_rcnn I only use global range(127720, 187540) 
+    # because in faster_rcnn I only use global range(127720, 187540)
     # but openpose is use all range(1, 359580)
     # so in there, i delete openpose's detections which is out of range
-    detection2 = [detection2[i] for i in range(0, len(detection2)) if detection2[i, 1] >= start and detection2[i, 1] <= end]
+    detection2 = [
+        detection2[i] for i in range(0, len(detection2))
+        if detection2[i, 1] >= start and detection2[i, 1] <= end
+    ]
 
+    img = show_detections(detection2, iCam, 122178)
+    cv2.imshow("video", img)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
