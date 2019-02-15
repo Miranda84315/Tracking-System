5f4d7f27c686499fbe532c5383d919b86460d347
Miranda84315 <miranda84315@gmail.com>
2019-02-15 15:53:09 +0800

    add openpose

--- src/detection/adaptive_detector/adaptive_detector.py
+++ src/detection/adaptive_detector/adaptive_detector.py
@@ -20,12 +20,6 @@ PartFrames = [
 start_sequence = 127720
 end_sequence = 187540
 
-
-def cal_localtime(icam, frame_num):
-    # get the real locat time
-    return start_sequence + frame_num - start_time[icam - 1] + 1
-
-
 def calucate_part(icam, frame):
     sum_frame = 0
     for part_num in range(0, 10):
@@ -47,30 +41,42 @@ def load_detection_h5py(detection_path):
     return data
 
 
+def draw_pose(pose, img):
+
+
+
 def show_detections(detection, iCam, frame):
+    part_cam, part_frame = calucate_part(iCam, frame)
+    filename = 'D:/Code/DukeMTMC/videos/camera' + str(iCam) + '/0000' + str(part_cam) + '.MTS'
+    cap = cv2.VideoCapture(filename)
+    cap.set(1, part_frame)
+    _, img = cap.read()
+    # cv2.imshow("video", img)
+    # cv2.waitKey(1)
+    detections = [detection[i] for i in range(0, len(detection)) if detection[i][1] == frame]
+    img_pose = draw_pose(detections, img)
+
+    return img_pose
 
 
+# def adaptive_detector(detection1, detection2):
+def main():
+    iCam = 1
+    detection1_path = detection_root[0] + 'camera' + str(iCam) + '.mat'
+    detection2_path = detection_root[1] + 'camera' + str(iCam) + '.mat'
 
-def adaptive_detector(detection1, detection2):
+    detection1 = load_detection(detection1_path)
+    detection2 = load_detection_h5py(detection2_path)
 
+    start = start_sequence - start_time[iCam - 1] + 1
+    end = end_sequence - start_time[iCam - 1] + 1
 
+    # because in faster_rcnn I only use global range(127720, 187540) 
+    # but openpose is use all range(1, 359580)
+    # so in there, i delete openpose's detections which is out of range
+    detection2 = [detection2[i] for i in range(0, len(detection2)) if detection2[i, 1] >= start and detection2[i, 1] <= end]
 
-
-iCam = 1
-detection1_path = detection_root[0] + 'camera' + str(iCam) + '.mat'
-detection2_path = detection_root[1] + 'camera' + str(iCam) + '.mat'
-
-detection1 = load_detection(detection1_path)
-detection2 = load_detection_h5py(detection2_path)
-
-start = start_sequence - start_time[iCam - 1] + 1
-end = end_sequence - start_time[iCam - 1] + 1
-
-# because in faster_rcnn I only use global range(127720, 187540) 
-# but openpose is use all range(1, 359580)
-# so in there, i delete openpose's detections which is out of range
-detection2 = [detection2[i] for i in range(0, len(detection2)) if detection2[i, 1] > start and detection2[i, 1] < end]
-
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
