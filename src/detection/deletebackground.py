9c8e4f21987a43a6fcb08de9e27f7c56e7e9dff8
Miranda84315 <miranda84315@gmail.com>
2019-03-22 22:17:05 +0800

    finish image alignment

--- src/detection/deletebackground.py
+++ src/detection/deletebackground.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import scipy.io
 import h5py
+from matplotlib import pyplot as plt
 
 start_time = [5543, 3607, 27244, 31182, 1, 22402, 18968, 46766]
 NumFrames = [359580, 360720, 355380, 374850, 366390, 344400, 337680, 353220]
@@ -20,6 +21,8 @@ end_sequence = 187540
 
 openpose_root = 'D:/Code/TrackingSystem/dataset/detections/openpose_bb/camera'
 background_root = 'D:/Code/TrackingSystem/dataset/background/bg_cam'
+save_root = 'D:/Code/TrackingSystem/dataset/detections/openpose_bg/camera'
+score_save = 'D:/Code/TrackingSystem/dataset/detections/openpose_bg/score'
 
 def calucate_part(icam, frame):
     sum_frame = 0
@@ -57,22 +60,119 @@ def get_detection(icam, frame, left, top, right, bottom):
     return img
 
 
+def similarity_img(img1, img2):
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    return np.sum(np.absolute(img1 - img2) < 30) / (len(img1[0])*len(img1)) / 255
+
+
+def draw_hsv(flow):
+    h, w = flow.shape[:2]
+    fx, fy = flow[:, :, 0], flow[:, :, 1]
+    ang = np.arctan2(fy, fx) + np.pi
+    v = np.sqrt(fx*fx+fy*fy)
+    hsv = np.zeros((h, w, 3), np.uint8)
+    hsv[..., 0] = ang*(180/np.pi/2)
+    hsv[..., 1] = 255
+    hsv[..., 2] = cv2.normalize(v, None, 0, 255, cv2.NORM_MINMAX)
+    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    cv2.imshow('frame2', bgr)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return bgr
+
+
+def warp_flow(img, flow):
+    h, w = flow.shape[:2]
+    flow = -flow
+    flow[:, :, 0] += np.arange(w)
+    flow[:, :, 1] += np.arange(h)[:, np.newaxis]
+    res = cv2.remap(img, flow, None, cv2.INTER_LINEAR)
+    cv2.imshow('remap', res)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return res
+
+
+def mse(imageA, imageB):
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
+    print(err)
+    return err
+
+
+def sift_kp(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    sift = cv2.xfeatures2d_SIFT.create()
+    kp, des = sift.detectAndCompute(image, None)
+    kp_image = cv2.drawKeypoints(gray_image, kp, None)
+    return kp_image, kp, des
+
+
+def get_good_match(des1, des2):
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+    good = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good.append(m)
+    return good
+
+
+def siftImageAlignment(img1, img2):
+    _, kp1, des1 = sift_kp(img1)
+    _, kp2, des2 = sift_kp(img2)
+    goodMatch = get_good_match(des1, des2)
+    imgOut = 0
+    flag = False
+    if len(goodMatch) > 4:
+        ptsA = np.float32([kp1[m.queryIdx].pt for m in goodMatch]).reshape(-1, 1, 2)
+        ptsB = np.float32([kp2[m.trainIdx].pt for m in goodMatch]).reshape(-1, 1, 2)
+        ransacReprojThreshold = 4
+        H, status = cv2.findHomography(ptsA, ptsB, cv2.RANSAC, ransacReprojThreshold)
+        if H is not None:
+            imgOut = cv2.warpPerspective(img2, H, (img1.shape[1], img1.shape[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+            flag = True
+    return imgOut, flag
+
+
 def main():
     icam = 1
     detections = load_mat(icam)
-
     bg = get_bg(icam)
+    detections = np.hstack((detections, np.ones((len(detections), 1))))
+    score = []
     for k in range(0, len(detections)):
+        print('----- detection = ', k, ' / ', len(detections))
         frame = int(detections[k, 1])
         left = int(detections[k, 2])
         top = int(detections[k, 3])
         right = int(detections[k, 2] + detections[k, 4])
         bottom = int(detections[k, 3] + detections[k, 5])
-        detection_bg = bg[top:bottom, left:right]
-        detection_img = get_detection(icam, frame, left, top, right, bottom)
+        if(detections[k, 4] < 20 or detections[k, 5] < 20 or detections[k, 5] > 450):
+            detections[k, 6] = 0
+        else:
+            if(bottom < 1080 and right < 1920 and left >= 30 and top >= 30):
+                detection_bg = bg[top:bottom, left:right]
+                detection_img = get_detection(icam, frame, left, top, right, bottom)
 
-        cv2.imshow("video", detection_bg)
-        cv2.waitKey(1)
+                result, flag = siftImageAlignment(detection_img, detection_bg)
+                if flag is True:
+                    height = detection_img.shape[0]
+                    width = detection_img.shape[1]
+                    detection_bg = detection_bg[10:height-20, 10:width-20]
+                    detection_img = detection_img[10:height-20, 10:width-20]
+                    result = result[10:height-20, 10:width-20]
+                    #allImg = np.concatenate((detection_bg, detection_img, result), axis=1)
+                    #cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
+                    #cv2.imshow('Result', allImg)
+                    #cv2.waitKey(0)
+                    score.append(mse(detection_img, result))
+                    if mse(detection_img, result) < 5000:
+                        detections[k, 6] = 0
+    print(score)
+    np.savetxt(score_save + str(icam) + '.txt', score)
+    scipy.io.savemat(save_root + str(icam) + '.mat', mdict={'detections': detections})
 
 
 cv2.destroyAllWindows()
@@ -80,3 +180,17 @@ cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
+
+'''
+            #img1 = cv2.cvtColor(detection_bg, cv2.COLOR_BGR2GRAY)
+            #img2 = cv2.cvtColor(detection_img, cv2.COLOR_BGR2GRAY)
+            #flow = cv2.calcOpticalFlowFarneback(img2, img1, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            #hsv = draw_hsv(flow)
+            #result = warp_flow(img2, flow)
+            #score.append(mse(img1, img2))
+            #img_diff = cv2.absdiff(detection_bg, detection_img)
+            #cv2.imshow('frame1', img1)
+            #cv2.imshow('frame2', img2)
+            #cv2.imshow('diff', img_diff)
+            #cv2.waitKey(0)
+'''
